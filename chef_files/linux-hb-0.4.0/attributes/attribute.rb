6a464f325bc3b1a76ee2d32fbaad55a565b22865
muralindia <muralindia@gmail.com>
2018-12-01 12:55:21 +0530

    updated additional line example

--- chef_files/linux-hb-0.4.0/attributes/attribute.rb
+++ chef_files/linux-hb-0.4.0/attributes/attribute.rb
@@ -2,4 +2,4 @@ default['Cron_dynamic']['cron_default'] = "*/5 * * * *"
 default['Cron_dynamic']['blank'] = " "
 
 
-default["userinto"]["users"] = %w{user1 user2 user3}
+default["eatable"]["items"] = ['bacon0', 'eggs0', 'sausage0']
